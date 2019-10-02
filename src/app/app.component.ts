a948fa9e7d6237a57b1848f7aa9b4723f54af56e
Jonas Langford <jonas.langford@protonmail.ch>
2019-10-02 11:58:32 +0200

    [SETUP] add breakpoint observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import {AuthService} from './authentication/service/auth.service';
 import {Router} from '@angular/router';
 import {authRoutesNames} from './authentication/auth.route.names';
 
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,8 +14,20 @@ import {authRoutesNames} from './authentication/auth.route.names';
 export class AppComponent {
   title = 'projektZwo';
 
-  constructor(public router: Router, public authService: AuthService) {
+  constructor(public router: Router, public authService: AuthService, public breakpointObserver: BreakpointObserver) {
 
+  }
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(min-width: 500px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          console.log('Viewport is 500px or over!');
+        } else {
+          console.log('Viewport is getting smaller!');
+        }
+      });
   }
 
   logout() {
